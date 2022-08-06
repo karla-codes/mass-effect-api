611dfef6c85866cfc3c8e0c3636f9f0279dbf7ea
karla-walker <indiablonix@gmail.com>
2022-08-06 13:06:56 -0700

    Move index file to src folder

--- index.ts
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-
-// middleware
-app.use(express.json())
-
-// port connection
-const port = process.env.PORT || 5500
-app.listen(port, () => console.log(`Listening on port: ${port}`))
