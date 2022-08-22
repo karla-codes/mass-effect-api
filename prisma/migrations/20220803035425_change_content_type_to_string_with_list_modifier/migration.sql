2e8fd5b247527c435798d8d6da995c42b610dc43
karla-walker <indiablonix@gmail.com>
2022-08-22 00:14:58 -0700

    Delete old files

--- prisma/migrations/20220803035425_change_content_type_to_string_with_list_modifier/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `content` column on the `Entry` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Entry" DROP COLUMN "content",
-ADD COLUMN     "content" TEXT[];
