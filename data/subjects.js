b228d3b43fd5d224b3fa8caf431dbb755fb2d529
karla-walker <indiablonix@gmail.com>
2022-08-22 00:11:21 -0700

    Create subject id data

---
+++ data/subjects.js
@@ -0,0 +1,64 @@
+const subjects = [
+  {
+    name: "Aliens: Council Races",
+    id: 1,
+  },
+  {
+    name: "Aliens: Extinct Races",
+    id: 2,
+  },
+  {
+    name: "Aliens: Non-Council Races",
+    id: 3,
+  },
+  {
+    name: "Aliens: Non-Sapient Creatures",
+    id: 4,
+  },
+  {
+    name: "Citadel and Galactic Government",
+    id: 5,
+  },
+  {
+    name: "Humanity and the Systems Alliance",
+    id: 6,
+  },
+  {
+    name: "Known Associates",
+    id: 7,
+  },
+  {
+    name: "Organizations",
+    id: 8,
+  },
+  {
+    name: "Personal History Summary",
+    id: 9,
+  },
+  {
+    name: "Planets and Locations",
+    id: 10,
+  },
+  {
+    name: "Publications",
+    id: 11,
+  },
+  {
+    name: "Ships and Vehicles",
+    id: 12,
+  },
+  {
+    name: "Technology",
+    id: 13,
+  },
+  {
+    name: "The Reapers",
+    id: 14,
+  },
+  {
+    name: "Weapons, Armor and Equipment",
+    id: 15,
+  },
+]
+
+module.exports = subjects
