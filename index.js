69d49b5ffa99dc1e5580280f5c9f1c549f58eb99
karla-walker <indiablonix@gmail.com>
2023-01-20 23:51:58 -0800

    Update origin url

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ app.use(
 )
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*") // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://63cb9922f570e50009c5aed8--sparkly-sunburst-3abda9.netlify.app"
+  ) // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
