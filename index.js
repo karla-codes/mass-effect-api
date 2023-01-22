ae9a65bbf45218858924a1dc7c4d5099175943aa
karla-walker <indiablonix@gmail.com>
2023-01-21 17:31:39 -0800

    Remove brackets from origin header

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"])
+  res.append("Access-Control-Allow-Origin", "*")
   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
   res.append("Access-Control-Allow-Headers", "Content-Type")
   next()
