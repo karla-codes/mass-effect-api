557d0bb2e2579582dfa16f49d47ebd1c73552bf6
karla-walker <indiablonix@gmail.com>
2023-01-21 16:54:57 -0800

    Add preflightContinue cors option

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ const app = express()
 
 // middleware
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    preflightContinue: true,
+  })
+)
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*") // update to match the domain you will make the request from
